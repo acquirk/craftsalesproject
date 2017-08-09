f401d2a4551b272f0c258d1cd5f01437e897ab7c
alaszlo <alaszlo7@gmail.com>
2017-08-09 12:10:16 -0400

    form connections

--- app_client/reports/reports.controller.js
+++ app_client/reports/reports.controller.js
@@ -11,14 +11,12 @@
 
         vm.customers = [];
         vm.names = [];
-<<<<<<< HEAD
         vm.sales = [];
-    
+
     meanData.salesGrab()
-=======
 
     meanData.accountsGrab()
->>>>>>> reportupload
+
       .success(function(data) {
         vm.customers = data;
         var k = 0;
@@ -85,6 +83,24 @@
                     $location.path('dashboard');
                 });
         };
+
+        var app = angular.module('angularjs-starter', []);
+
+          app.controller('MainCtrl', function($scope) {
+
+            $scope.choices = [{id: 'choice1'}, {id: 'choice2'}];
+
+            $scope.addNewChoice = function() {
+              var newItemNo = $scope.choices.length+1;
+              $scope.choices.push({'id':'choice'+newItemNo});
+            };
+
+            $scope.removeChoice = function() {
+              var lastItem = $scope.choices.length-1;
+              $scope.choices.splice(lastItem);
+            };
+
+          });
 
     }
 

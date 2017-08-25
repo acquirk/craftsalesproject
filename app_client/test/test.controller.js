2c37c2f6ea82a320df2c1d529d1d9774ae4a2b38
andrewlaszlo <alaszlo7@gmail.com>
2017-08-25 17:44:46 -0400

    Wheee Testing

--- app_client/test/test.controller.js
+++ app_client/test/test.controller.js
@@ -8,6 +8,19 @@
   function testCtrl($location, authentication, meanData) {
     var vm = this;
     vm.test = "hello world";
+
+    vm.spirits = ['Songbird Coffee', 'Flora', 'Lakehouse', 'Tiki Rum', 'Gin', 'Bramble', 'Vodka'];
+
+    vm.projects = [['Cardinal Spirits Website','2016'],['Spirit Finder','2017'],['Quirk Adventures','2015'],['Foundation: Wordpress Theme', '2014'],['bespoke','2013']];
+
+    vm.places = ['Bloomington', 'Space', 'Cincinnati'];
+
+    vm.pic = 'bloomington';
+
+    vm.click = function () {
+
+
+    }
 }
-                                      
+
 })();
